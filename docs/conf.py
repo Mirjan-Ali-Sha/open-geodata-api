b7ae1ca3d78d98a329a57e1c7b6a0a7b24b2eb0a
Mirjan Ali Sha <mirjanalisha@gmail.com>
2025-06-21 04:32:52 +0530

    adding full documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,62 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-
+# docs/conf.py
 import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-# -- Project information -----------------------------------------------------
-project = 'Open Geodata API'
-copyright = '2025, Your Name'
-author = 'Your Name'
+# Check if we're building on Read the Docs
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
-
-# -- General configuration ---------------------------------------------------
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.extlinks',
-    'sphinx_copybutton',
-    'myst_parser',
-    'nbsphinx',
-]
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-
-# Autodoc settings
-autodoc_typehints = 'description'
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'private-members': False,
-    'special-members': '__init__',
-    'inherited-members': True,
-    'show-inheritance': True,
-}
-
-# Intersphinx mapping
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None),
-    'geopandas': ('https://geopandas.org/en/stable/', None),
-    'rasterio': ('https://rasterio.readthedocs.io/en/latest/', None),
-}
-
-# Copy button configuration
-copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
-copybutton_prompt_is_regexp = True
+if on_rtd:
+    # RTD-specific configuration
+    html_theme = 'sphinx_rtd_theme'
+    html_context = {
+        'display_github': True,
+        'github_user': 'yourusername',
+        'github_repo': 'open-geodata-api',
+        'github_version': 'main',
+        'conf_py_path': '/docs/',
+    }
+else:
+    # Local development configuration
+    html_theme = 'sphinx_rtd_theme'
